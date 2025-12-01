9ab5870cfbb0add279f5c1f653f894b42de3ebbc
Kudakwashe Jasi <kudakwashejasi4@gmail.com>
2025-12-01 03:46:31 +0200

    fix: ESLint config for test files and update Node.js version in CI/CD

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -5,14 +5,22 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  // Ignore default folders
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+
+    files: ["**/*.spec.ts", "**/test/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
